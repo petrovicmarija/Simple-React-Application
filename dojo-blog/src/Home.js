7b7839d74ac09516649e50df031ee3e127006cb6
Marija Petrovic <marijap@protonmail.com>
2021-08-12 14:11:54 +0200

    deleting blogs

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,25 +1,21 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+        { title: 'Welcome party', body: 'lorem ipsum...', author: 'yoshi', id: 2},
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
+    ]);
 
-    //let name = 'john';
-    const [name, setName] = useState('john');
-    const [age, setAge] = useState(25);
-
-    const handleClick = () => {
-        setName('matt');
-        setAge(30);
-    }
-
-    const handleClickAgain = (name) => {
-        console.log('hello ' + name);
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
     }
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p> { name } is { age } years old </p>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={() => handleClickAgain('john') }>Click me again</button>
+            <BlogList blogs={ blogs } title="All Blogs" handleDelete={handleDelete} />
+            {/* <BlogList blogs={ blogs.filter((blog) => blog.author === 'mario') } title="Mario's blogs" /> */}
         </div>
       );
 }
